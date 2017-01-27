c18f348817312357611ee6f59f3cb719a69f06d0
Imdad Ali <imdad.ali@live.co.uk>
2017-01-27 20:34:02 +0000

    added settings placements of files. Initialised some variables such as recycler view

--- app/src/main/java/com/bluelead/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/bluelead/popularmovies/DetailsActivity.java
@@ -1,14 +1,41 @@
 package com.bluelead.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = DetailsActivity.this;
+        Intent activityIntent;
+        Class destinationActivity;
+
+        if(item.getItemId() == R.id.settingsOption) {
+            destinationActivity = SettingsActivity.class;
+            activityIntent = new Intent(context, destinationActivity);
+            startActivity(activityIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/bluelead/popularmovies/MainActivity.java
+++ app/src/main/java/com/bluelead/popularmovies/MainActivity.java
@@ -1,13 +1,51 @@
 package com.bluelead.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    GridLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_movieGrid);
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = MainActivity.this;
+        Intent activityIntent;
+        Class destinationActivity;
+
+        if(item.getItemId() == R.id.settingsOption) {
+            destinationActivity = SettingsActivity.class;
+            activityIntent = new Intent(context, destinationActivity);
+            startActivity(activityIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
