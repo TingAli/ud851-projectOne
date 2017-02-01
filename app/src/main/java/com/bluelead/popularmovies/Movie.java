8c8275e2289407992d851a8b0704c848f6871c69
Imdad Ali <imdad.ali@live.co.uk>
2017-02-01 19:50:24 +0000

    adding google gson

--- app/src/main/java/com/bluelead/popularmovies/Movie.java
+++ app/src/main/java/com/bluelead/popularmovies/Movie.java
@@ -1,26 +1,136 @@
 package com.bluelead.popularmovies;
 
-import java.util.Date;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by Imdad Ali on 28/01/2017.
  */
 
-public class Movie {
-    private int mMovieId;
-    private String mTitle;
-    private String mMoviePosterPath;
-    private String mOverview;
-    private Double mUserRating;
-    private Date mReleaseDate;
+public class Movie implements Parcelable {
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
 
-    public Movie(int movieId, String title, String moviePosterPath, String overview, Double userRating,
-                 Date releaseDate){
-        mMovieId = movieId;
-        mTitle = title;
-        mMoviePosterPath = moviePosterPath;
-        mOverview = overview;
-        mUserRating = userRating;
-        mReleaseDate = releaseDate;
+        public Movie createFromParcel(Parcel in) {
+
+            return new Movie(in);
+        }
+
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+    private String overview;
+    private String title;
+    @SerializedName("poster_path")
+    private String posterPath;
+    @SerializedName("vote_average")
+    private String voteAvarage;
+    @SerializedName("release_date")
+    private String releaseDate="";
+    private String id;
+    private int runtime;
+    private String cachedPosterPath;
+
+    public Movie() {
+
+    }
+
+    // parcel construtor
+    protected Movie(Parcel p) {
+        id = p.readString();
+        overview = p.readString();
+        posterPath = p.readString();
+        title = p.readString();
+        voteAvarage = p.readString();
+        releaseDate=p.readString();
+        runtime=p.readInt();
+        cachedPosterPath=p.readString();
+    }
+
+    public String getCachedPosterPath() {
+        return cachedPosterPath;
+    }
+
+    public void setCachedPosterPath(String cachedPosterPath) {
+        this.cachedPosterPath = cachedPosterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVoteAvarage() {
+        return voteAvarage;
+    }
+
+    public void setVoteAvarage(String voteAvarage) {
+        this.voteAvarage = voteAvarage;
+    }
+
+    public String getReleaseYear(){
+        return releaseDate.substring(0,4);
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(overview);
+        dest.writeString(posterPath);
+        dest.writeString(title);
+        dest.writeString(voteAvarage);
+        dest.writeString(releaseDate);
+        dest.writeInt(runtime);
+        dest.writeString(cachedPosterPath);
     }
 }
--- app/src/main/java/com/bluelead/popularmovies/MovieNetworkUtils.java
+++ app/src/main/java/com/bluelead/popularmovies/MovieNetworkUtils.java
@@ -9,8 +9,9 @@ import android.content.Context;
 public class MovieNetworkUtils {
     private final static String POSTER_BASE_URL = " http://image.tmdb.org/t/p/";
     private final static String POSTER_SIZE = "w185";
+    private final static String KEY_PARAM = "api_key";
 
-    private final static String API_URL = "http://api.themoviedb.org/3/movie/popular?api_key=";
+    private final static String API_BASE_URL = "http://api.themoviedb.org/3";
     private final static String API_KEY = "08f5e8f842b74e8eee405e07bc06c86c";
 
 
